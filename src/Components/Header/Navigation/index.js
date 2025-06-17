ec6b31056b6d356057fd479214f2fe97c93712fc
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2025-06-16 21:32:08 -0400

    added slider

--- src/Components/Header/Navigation/index.js
+++ src/Components/Header/Navigation/index.js
@@ -3,24 +3,43 @@ import { FaAngleDown } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { CiHome } from "react-icons/ci";
 import { Button } from '@mui/material';
+import { useState } from 'react';
 
 const Navigation = () => {
+    const [isopenSidebarVal, setisopenSidebarVal] = useState(true);
     return(
         <nav>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-sm-3 navPart1'>
-                        <button className='allCatTab align-items-center'>
-                            <span className='icon1 mr-2'><IoListOutline/></span>
-                            <span className="text">ALL CATEGORIES</span>
-                            <span className='icon2 ml-2'><FaAngleDown/></span>
+                <div className="col-sm-3 navPart1">
+                    <div className="catWrapper">
+                        <button className="allCatTab align-items-center"
+                        onClick={()=>setisopenSidebarVal(!isopenSidebarVal)}>
+                        <span className="icon1 mr-2"><IoListOutline /></span>
+                        <span className="text">ALL CATEGORIES</span>
+                        <span className="icon2 ml-2"><FaAngleDown /></span>
                         </button>
+
+                        <div className={`sidebarNav ${isopenSidebarVal===true ? 'open' : ''}`}>
+                        <ul>
+                            <li><Link to="/"><Button>Black</Button></Link></li>
+                            <li><Link to="/"><Button>Black</Button></Link></li>
+                            <li><Link to="/"><Button>Black</Button></Link></li>
+                            <li><Link to="/"><Button>Black</Button></Link></li>
+                        </ul>
+                        </div>
+                    </div>
                     </div>
                     <div className='col-sm-9 navPart2'>
                         <ul className = 'list list-inline ml-auto'>
                             <li className = 'list-inline-item'><Link to="/"><Button><CiHome/>&nbsp; Home
                             </Button></Link></li>
-                            <li className = 'list-inline-item'><Link to="/"><Button>Shop</Button></Link></li>
+                            <li className = 'list-inline-item'><Link to="/"><Button>Shop</Button></Link>
+                            <div className="submenu">
+                                <Link to="/"><Button>Black</Button></Link>
+                                <Link to="/"><Button>White</Button></Link>
+                                <Link to="/"><Button>Red</Button></Link>
+                                </div></li>
                             <li className = 'list-inline-item'><Link to="/"><Button>Iphone</Button></Link></li>
                             <li className = 'list-inline-item'><Link to="/"><Button>Parts</Button></Link></li>
                             <li className = 'list-inline-item'><Link to="/"><Button>Repairs</Button></Link></li>
---
+++ src/Components/HomeBanner/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Slider from "react-slick";
+
+
+const HomeBanner = () => {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+    return(
+        <div className="homeBannerSection">
+            <Slider {...settings}>
+                <div className="item">
+                    <img src="https://sslimages.shoppersstop.com/sys-master/root/h1e/h9b/32374912352286/Static-Web---2024-04--01-new-hp-page.jpg" className="w-100"/>
+                </div>
+                <div className="item">
+                    <img src="https://sslimages.shoppersstop.com/sys-master/root/h1e/h9b/32374912352286/Static-Web---2024-04--01-new-hp-page.jpg" className="w-100"/>
+                </div>
+                <div className="item">
+                    <img src="https://sslimages.shoppersstop.com/sys-master/root/h1e/h9b/32374912352286/Static-Web---2024-04--01-new-hp-page.jpg" className="w-100"/>
+                </div>
+                <div className="item">
+                    <img src="https://sslimages.shoppersstop.com/sys-master/root/h1e/h9b/32374912352286/Static-Web---2024-04--01-new-hp-page.jpg" className="w-100"/>
+                </div>
+            </Slider>
+        </div>
+    )
+};
+
+export default HomeBanner;
